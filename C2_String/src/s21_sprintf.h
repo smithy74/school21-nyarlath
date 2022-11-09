8fe625c761204c265cc80bb548db12ff0bb8faf6
Kuznetsova Kristina <101335982+smithy74@users.noreply.github.com>
2022-11-09 23:44:36 +0300

    C2_String - Обработка парсеров

---
+++ C2_String/src/s21_sprintf.h
@@ -0,0 +1,9 @@
+#ifndef SRC_S21_SPRINTF_H
+#define SRC_S21_SPRINTF_H
+
+#include <stdio.h>
+#include <stdarg.h>
+
+int s21_sprintf(char *str, const char *format, ...);
+
+#endif  // SRC_S21_SPRINTF_H
--- C2_String/src/s21_string.h
+++
@@ -1,5 +0,0 @@
-#ifdef SRC_S21_STRING_H
-#define SRC_S21_STRING_H
-
-
-#endif  // SRC_S21_STRING_H
