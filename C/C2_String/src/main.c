af9c5411d58f3c9df4b728d79ad2fb1f04fc8859
Kuznetsova Kristina <101335982+smithy74@users.noreply.github.com>
2022-12-03 16:38:14 +0300

    Изменение папок
    -Чистка проекта Стринги
    - Добавление бассейна по питону

--- C/C2_String/src/main.c
+++
@@ -1,8 +0,0 @@
-#include "s21_sprintf.h"
-#include <stdio.h>
-
-int main(void) {
-    char buffer[256];
-    sprintf();
-    s21_sprintf(buffer, "%c%d%i%e%E%f%g%G%o%s%u%x%X%p%n");
-}
--- C/C2_String/src2/s21_sprintf.h
+++
@@ -1,9 +0,0 @@
-#ifndef SRC_S21_SPRINTF_H
-#define SRC_S21_SPRINTF_H
-
-#include <stdio.h>
-#include <stdarg.h>
-
-int s21_sprintf(char *str, const char *format, ...);
-
-#endif  // SRC_S21_SPRINTF_H
